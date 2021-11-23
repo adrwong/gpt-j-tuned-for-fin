a46b3ee29b8c66603bca6d1df2f11cec2b29ea19
adrwong <wongsiuho561@gmail.com>
2021-11-23 12:10:34 +0800

    updated inference scripts

--- generation_recurring_input_trial.py
+++ generation_recurring_input_trial.py
@@ -7,14 +7,12 @@ tokenizer = AutoTokenizer.from_pretrained('../../tuned_to_finbase2_slim_hf', loc
 # tokenizer = GPT2Tokenizer.from_pretrained('EleutherAI/gpt-j-6B')
 # model = GPTJModel.from_pretrained('../../tuned_to_finbase2_slim_hf', local_files_only=True)
 
-for line in sys.stdin:
-    if 'q' == line.rstrip():
-        break
-    print(f'Input : {line}')
-    input_ids = tokenizer(line, return_tensors="pt").input_ids
 
-    gen_tokens = model.generate(input_ids, do_sample=True, temperature=0.9, max_length=100,)
-    gen_text = tokenizer.batch_decode(gen_tokens)[0]
-    print(gen_text)
+input_ids = tokenizer("Hello, my dog is cute", return_tensors="pt").input_ids
+
+gen_tokens = model.generate(input_ids, do_sample=True, temperature=0.9, max_length=100,)
+gen_text = tokenizer.batch_decode(gen_tokens)[0]
+
+print(gen_text)
  
 print("Exit")
